cc11108062fd9ab53905188e24c977b82afe780e
Stefan Horst <stefan@ultrachaos.de>
2009-07-16 16:45:18 +0000

    added new checksumms

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sh_scoutnet_webservice"
 #
-# Auto generated 16-07-2009 18:10
+# Auto generated 16-07-2009 18:42
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"cc0e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"5eda";s:19:"doc/wizard_form.dat";s:4:"c6c9";s:20:"doc/wizard_form.html";s:4:"81fe";s:41:"sv1/class.tx_shscoutnetwebservice_sv1.php";s:4:"21aa";}',
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"cc0e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"5eda";s:50:"sn/class.tx_shscoutnetwebservice_jsonRPCClient.php";s:4:"d555";s:39:"sn/class.tx_shscoutnetwebservice_sn.php";s:4:"f9c6";s:19:"doc/wizard_form.dat";s:4:"c6c9";s:20:"doc/wizard_form.html";s:4:"81fe";}',
+	'suggests' => array(
+	),
 );
 
 ?>

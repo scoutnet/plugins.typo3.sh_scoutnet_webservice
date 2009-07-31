e176af123d0a838519a652a9be251bcb046028f7
Stefan Horst <stefan@ultrachaos.de>
2009-07-31 08:51:06 +0000

    sammel

--- sn/models/Events.php
+++ sn/models/Events.php
@@ -7,7 +7,7 @@ class SN_Model_Event extends ArrayObject{
 
 	public function get_Author_name(){
 		if (isset($this['Author']) && $this['Author'] != null) {
-			return (string) htmlentities(utf8_decode($this['Author']->get_long_Name()));
+			return (string) htmlentities(utf8_decode($this['Author']->get_full_Name()));
 		}
 
 		return (string) "";

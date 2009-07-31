b3a0abbbc070e8a3f34e1b0da9a5b2a9ca31857b
Stefan Horst <stefan@ultrachaos.de>
2009-07-31 08:40:41 +0000

    sammel

--- sn/models/Events.php
+++ sn/models/Events.php
@@ -12,5 +12,18 @@ class SN_Model_Event extends ArrayObject{
 
 		return (string) "";
 	}
+
+	public function get_Stufen_Images() {
+		if (isset($this['Stufen']) && $this['Stufen'] != null) {
+			
+			$stufen = "";
+			foreach ($this['Stufen'] as $stufe) { 
+				$stufen .= $stufe->get_Image_URL();
+			}
+
+			return (string) $stufen;
+		}
+		return (string) "";
+	}
 }
 

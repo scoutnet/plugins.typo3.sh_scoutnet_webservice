62aed1cba023bdde17564fa294d7ebeb5866448a
Stefan Horst <stefan@ultrachaos.de>
2009-09-28 18:21:37 +0000

    added new field

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "sh_scoutnet_webservice"
 #
-# Auto generated 09-09-2009 09:50
+# Auto generated 28-09-2009 20:19
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.9',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"cc0e";s:10:"README.txt";s:4:"248f";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"5eda";s:50:"sn/class.tx_shscoutnetwebservice_jsonRPCClient.php";s:4:"d555";s:39:"sn/class.tx_shscoutnetwebservice_sn.php";s:4:"4903";s:28:"sn/models/SN_Model_Event.php";s:4:"2a66";s:31:"sn/models/SN_Model_Kalender.php";s:4:"e9d5";s:28:"sn/models/SN_Model_Stufe.php";s:4:"ddd8";s:27:"sn/models/SN_Model_User.php";s:4:"3d31";s:19:"doc/wizard_form.dat";s:4:"c6c9";s:20:"doc/wizard_form.html";s:4:"81fe";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"cc0e";s:10:"README.txt";s:4:"248f";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"5eda";s:50:"sn/class.tx_shscoutnetwebservice_jsonRPCClient.php";s:4:"d555";s:39:"sn/class.tx_shscoutnetwebservice_sn.php";s:4:"57dd";s:28:"sn/models/SN_Model_Event.php";s:4:"2a66";s:31:"sn/models/SN_Model_Kalender.php";s:4:"63f5";s:28:"sn/models/SN_Model_Stufe.php";s:4:"ddd8";s:27:"sn/models/SN_Model_User.php";s:4:"3d31";s:19:"doc/wizard_form.dat";s:4:"c6c9";s:20:"doc/wizard_form.html";s:4:"81fe";}',
 	'suggests' => array(
 	),
 );

51b8beeead7292126fe7f7a64ee6689af676e2b3
Stefan Horst <stefan@ultrachaos.de>
2009-09-28 18:00:08 +0000

    added Description

--- sn/models/SN_Model_Kalender.php
+++ sn/models/SN_Model_Kalender.php
@@ -8,6 +8,10 @@ class SN_Model_Kalender extends ArrayObject{
 	public function get_long_Name() {
 		return (string) htmlentities(utf8_decode($this['Ebene'])).(($this['Ebene_Id'] >= 7)?"<br>".htmlentities(utf8_decode($this['Name'])):"");
 	}
+
+	public function get_Name() {
+		return (string) htmlentities(utf8_decode($this['Ebene'])).(($this['Ebene_Id'] >= 7)?"&nbsp;".htmlentities(utf8_decode($this['Name'])):"");
+	}
 }
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/sh_scoutnet_webservice/sn/models/SN_Model_Kalender.php'])	{

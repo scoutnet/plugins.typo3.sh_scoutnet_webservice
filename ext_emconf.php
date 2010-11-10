aa5ef56aceed40d205a8a4ac97fde967661931ca
Stefan Horst <stefan@ultrachaos.de>
2010-11-10 16:09:47 +0000

    changed the ex_conf

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.9.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 		),

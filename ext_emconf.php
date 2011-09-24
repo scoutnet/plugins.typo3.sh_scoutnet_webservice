86d3c8edf7777b51199e0186bde89a69a28b89d4
Stefan Horst <stefan@ultrachaos.de>
2011-09-24 14:58:48 +0000

    added new version config

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "sh_scoutnet_webservice".
 #
-# Auto generated 19-01-2011 11:58
+# Auto generated 24-09-2011 16:56
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.4',
+	'version' => '1.0.5',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"cc0e";s:10:"README.txt";s:4:"248f";s:21:"ext_conf_template.txt";s:4:"1505";s:12:"ext_icon.gif";s:4:"3ec8";s:17:"ext_localconf.php";s:4:"aafe";s:19:"doc/wizard_form.dat";s:4:"c6c9";s:20:"doc/wizard_form.html";s:4:"81fe";s:23:"res/scoutnetConnect.png";s:4:"ed5a";s:40:"sn/class.tx_shscoutnetwebservice_AES.php";s:4:"d24d";s:50:"sn/class.tx_shscoutnetwebservice_jsonRPCClient.php";s:4:"b164";s:39:"sn/class.tx_shscoutnetwebservice_sn.php";s:4:"d825";s:28:"sn/models/SN_Model_Event.php";s:4:"2a66";s:31:"sn/models/SN_Model_Kalender.php";s:4:"63f5";s:28:"sn/models/SN_Model_Stufe.php";s:4:"ddd8";s:27:"sn/models/SN_Model_User.php";s:4:"3d31";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"cc0e";s:10:"README.txt";s:4:"248f";s:21:"ext_conf_template.txt";s:4:"1505";s:12:"ext_icon.gif";s:4:"3ec8";s:17:"ext_localconf.php";s:4:"aafe";s:19:"doc/wizard_form.dat";s:4:"c6c9";s:20:"doc/wizard_form.html";s:4:"81fe";s:23:"res/scoutnetConnect.png";s:4:"ed5a";s:40:"sn/class.tx_shscoutnetwebservice_AES.php";s:4:"d24d";s:50:"sn/class.tx_shscoutnetwebservice_jsonRPCClient.php";s:4:"b164";s:39:"sn/class.tx_shscoutnetwebservice_sn.php";s:4:"da95";s:28:"sn/models/SN_Model_Event.php";s:4:"2a66";s:31:"sn/models/SN_Model_Kalender.php";s:4:"63f5";s:28:"sn/models/SN_Model_Stufe.php";s:4:"ddd8";s:27:"sn/models/SN_Model_User.php";s:4:"3d31";}',
 	'suggests' => array(
 	),
 );

0947454da4531278177c9fec225585c76ebcfb68
Stefan Horst <stefan@ultrachaos.de>
2011-09-24 11:28:54 +0000

    added some testcode

--- sn/class.tx_shscoutnetwebservice_sn.php
+++ sn/class.tx_shscoutnetwebservice_sn.php
@@ -182,6 +182,8 @@ class tx_shscoutnetwebservice_sn extends t3lib_svbase {
 	public function get_scoutnetConnectLoginButton($returnUrl = '',$requestApiKey = false){
 		$lang = 'de';
 
+		echo $GLOBALS['TSFE']->sys_language_content;
+
 		$this->_check_for_all_configValues();
 		$button = '<form action="'.$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['tx_shscoutnetwebservice']['ScoutnetLoginPage'].'" id="scoutnetLogin" method="post" target="_self">';
 

101d74357ee89d478c99671feefab63fa38fb1cd
Stefan Horst <stefan@ultrachaos.de>
2011-09-24 14:26:57 +0000

    added some testcode

--- sn/class.tx_shscoutnetwebservice_sn.php
+++ sn/class.tx_shscoutnetwebservice_sn.php
@@ -185,7 +185,7 @@ class tx_shscoutnetwebservice_sn extends t3lib_svbase {
 		$button = "<pre>";
 		//$button .= print_r($GLOBALS['BE_USER']->lang,true);
 		$button .= $GLOBALS['TSFE']->sys_language_uid;
-		$button .= print_r($GLOBALS['LANG']->LLkey,true);
+		$button .= print_r($GLOBALS['LANG']->getLL('header1Label'),true);
 		$botton .= "</pre>";
 
 		$this->_check_for_all_configValues();

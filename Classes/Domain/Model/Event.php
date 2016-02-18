fcff449bc6bee33864f21e1ad6a1c5b59d484822
Stefan Horst <stefan@ultrachaos.de>
2016-02-18 23:40:49 +0100

    fixed namespaces

--- Classes/Domain/Model/Event.php
+++ Classes/Domain/Model/Event.php
@@ -1,5 +1,37 @@
 <?php
-class SN_Model_Event extends ArrayObject{
+namespace ScoutNet\ShScoutnetWebservice\Domain\Model;
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2015 Stefan "Mütze" Horst <muetze@scoutnet.de>, ScoutNet
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Event
+ *
+ */
+//class Event extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+class Event extends ArrayObject{
 	function __construct( $array ){
 		parent::__construct($array);
 	}
@@ -25,8 +57,4 @@ class SN_Model_Event extends ArrayObject{
 		}
 		return (string) "";
 	}
-}
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/sh_scoutnet_webservice/sn/models/SN_Model_Event.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/sh_scoutnet_webservice/sn/models/SN_Model_Event.php']);
 }
--- Classes/Domain/Model/User.php
+++ Classes/Domain/Model/User.php
@@ -1,5 +1,35 @@
 <?php
-class SN_Model_User extends ArrayObject{
+namespace ScoutNet\ShScoutnetWebservice\Domain\Model;
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2015 Stefan "Mütze" Horst <muetze@scoutnet.de>, ScoutNet
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * User
+ */
+class User extends ArrayObject{
 	function __construct( $array ){
 		if(isset($array['id'])){
 			$array['userid'] = $array['id'];
@@ -88,8 +118,4 @@ class SN_Model_User extends ArrayObject{
 	public function get_contact_link_tag_first_name(){
 		return SN::instance()->get_users_contact_link_tag_first_name( $this );
 	}
-}
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/sh_scoutnet_webservice/sn/models/SN_Model_User.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/sh_scoutnet_webservice/sn/models/SN_Model_User.php']);
 }

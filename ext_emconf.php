d5970e76cd9c4b4ca0466f6cca0f5f1858dcd904
Stefan Horst <stefan@ultrachaos.de>
2016-11-10 22:28:37 +0100

    fixed em_conf

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,10 +22,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 1,
-	'version' => '2.0.0',
+	'version' => '2.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.0.99',
+			'typo3' => '6.2.0-7.6.99',
 		),
 		'conflicts' => array(
 		),

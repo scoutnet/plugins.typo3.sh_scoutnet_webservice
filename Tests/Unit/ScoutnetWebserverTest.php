d4af7ba2a594ed9be32250d3167809f747994f0f
Stefan Horst <stefan.horst@grandcentrix.net>
2017-02-21 19:49:29 +0100

    added UnitTests

---
+++ Tests/Unit/ScoutnetWebserverTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ScoutNet\ShScoutnetWebservice\Tests\Unit;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2017 Stefan Horst <stefan@ultrachaos.de>
+ *  All rights reserved
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+/**
+ * Testcase for class ScoutNet\ShScoutnetWebservice\Test
+ */
+class ScoutnetWebserviceTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+    /**
+     * @test
+     * @return void
+     */
+    public function myFirstTest() {
+        $this->assertEquals("test", "test");
+    }
+}

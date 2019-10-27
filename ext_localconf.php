b595419b33a7b3ea8b68d8b31011f4352ee90784
Stefan Horst <stefan@ultrachaos.de>
2019-10-27 13:37:10 -0400

    cleanup

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,13 @@
 <?php
+
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 if (!defined ('TYPO3_MODE')) {
  	die ('Access denied.');
 }
 
 
 // add converter for our objects
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter('ScoutNet\ShScoutnetWebservice\Property\TypeConverter\EventConverter');
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter('ScoutNet\ShScoutnetWebservice\Property\TypeConverter\StructureConverter');
+ExtensionUtility::registerTypeConverter('ScoutNet\ShScoutnetWebservice\Property\TypeConverter\EventConverter');
+ExtensionUtility::registerTypeConverter('ScoutNet\ShScoutnetWebservice\Property\TypeConverter\StructureConverter');
 

37a4542bf66bc906e2783c7b024785776aacf55b
Stefan Horst <stefan@ultrachaos.de>
2019-10-27 16:13:09 -0400

    cleanup

--- Classes/Helpers/ScoutNetConnectHelper.php
+++ Classes/Helpers/ScoutNetConnectHelper.php
@@ -67,7 +67,8 @@ class ScoutNetConnectHelper {
      */
 	public function getScoutNetConnectLoginButton($returnUrl = '',$requestApiKey = false){
 	    // TODO: use a template here!!
-		$lang = $GLOBALS['LANG']->lang;
+//		$lang = $GLOBALS['LANG']->lang;
+        $lang = $TSFE->sys_language_isocode;
 
 		if ($lang == 'default') 
 			$lang = 'en';

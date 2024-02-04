4930642fa994dd51dd0740d3aa9913c6e9c941b5
Stefan Horst <muetze@scoutnet.de>
2024-02-04 19:51:20 +0100

    fix emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '6.0.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '10.4.0-11.5.99',
+			'typo3' => '12.4.0-12.4.99',
 		],
 		'conflicts' => [],
 		'suggests' => [],
